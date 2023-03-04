f430f5164644ea005ff62e0146de3229ff1bf399
Lane Smith <ldsmith@uw.edu>
2023-03-04 01:25:15 -0800

    feat: update simple shiftable demand to allow a shiftable percent to be specified

--- src/read/read_demand.jl
+++ src/read/read_demand.jl
@@ -7,23 +7,13 @@ and return them in a Demand struct.
 function read_demand(filepath::String)::Demand
     # Initialize demand struct
     demand = Dict{String,Any}(
+        "demand_profile" => nothing,
         "simple_shift_enabled" => false,
-        "shift_capacity_profile" => nothing,
+        "shift_up_capacity_profile" => nothing,
+        "shift_down_capacity_profile" => nothing,
+        "shift_percent" => nothing,
         "shift_duration" => nothing,
     )
-
-    # Try loading the demand profile
-    try
-        demand["demand_profile"] =
-            DataFrames.DataFrame(CSV.File(joinpath(filepath, "demand_profile.csv")))
-        println("...loading demand profile")
-    catch e
-        throw(
-            ErrorException(
-                "Demand profile not found in " * filepath * ". Please try again.",
-            ),
-        )
-    end
 
     # Try loading the demand parameters
     try
@@ -35,7 +25,14 @@ function read_demand(filepath::String)::Demand
         # Try assigning the different demand parameters from the file
         for k in deleteat!(
             collect(keys(demand)),
-            findall(x -> x in ("shift_capacity_profile"), collect(keys(demand))),
+            findall(
+                x -> x in (
+                    "demand_profile",
+                    "shift_up_capacity_profile",
+                    "shift_down_capacity_profile",
+                ),
+                collect(keys(demand)),
+            ),
         )
             if !ismissing(demand_parameters[1, k])
                 demand[k] = demand_parameters[1, k]
@@ -49,6 +46,26 @@ function read_demand(filepath::String)::Demand
                 )
             end
         end
+
+        # Try loading the demand profile
+        try
+            demand["demand_profile"] = DataFrames.DataFrame(
+                CSV.File(
+                    joinpath(
+                        filepath,
+                        "demand_data",
+                        demand_parameters[1, "demand_file_name"],
+                    ),
+                ),
+            )
+            println("...loading demand profile")
+        catch e
+            throw(
+                ErrorException(
+                    "Demand profile not found in " * filepath * ". Please try again.",
+                ),
+            )
+        end
     catch e
         println(
             "Demand parameters not found in " *
@@ -59,21 +76,30 @@ function read_demand(filepath::String)::Demand
     end
 
     # Try loading the shiftable demand profile if shiftable demand is enabled
-    if demand["simple_shift_enabled"]
-        try
-            demand["shift_capacity_profile"] = DataFrames.DataFrame(
-                CSV.File(joinpath(filepath, "shiftable_demand_profile.csv")),
-            )
-            println("...loading shiftable demand profile")
-        catch e
-            println(
-                "Shiftable demand profile not found in " *
-                filepath *
-                ". Shiftable demand parameters will default to not allowing " *
-                "shiftable demand to be considered.",
-            )
-            demand["simple_shift_enabled"] = false
+    if demand["simple_shift_enabled"] & isnothing(demand["shift_percent"])
+        for i in ("up", "down")
+            try
+                demand["shift_" * i * "_capacity_profile"] = DataFrames.DataFrame(
+                    CSV.File(joinpath(filepath, "shiftable_demand_" * i * "_profile.csv")),
+                )
+                println("...loading shiftable demand " * i * " profile")
+            catch e
+                println(
+                    "Shiftable demand " *
+                    i *
+                    " profile not found in " *
+                    filepath *
+                    ". Shiftable demand parameters will default to not allowing " *
+                    "shiftable demand to be considered.",
+                )
+                demand["simple_shift_enabled"] = false
+            end
         end
+    end
+
+    # Check to make sure the demand profile is provided
+    if isnothing(demand["demand_profile"])
+        throw(ErrorException("No demand profile was provided. Please try again."))
     end
 
     # Convert Dict to NamedTuple
