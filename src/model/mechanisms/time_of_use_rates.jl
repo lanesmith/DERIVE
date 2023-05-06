881983c4c618f9a081786d41b5f085dd44ccc13d
lanesmith <lanesmith218@gmail.com>
2023-05-06 01:50:54 -0700

    docs: add docstrings to functions in time_of_use_rates.jl

--- src/model/mechanisms/time_of_use_rates.jl
+++ src/model/mechanisms/time_of_use_rates.jl
@@ -1,7 +1,9 @@
 """
     define_tou_energy_charge_onjective!(m::JuMP.Model, obj::JuMP.AffExpr, sets::Sets)
 
-TBW
+Adds a cost to the objective function for the time-of-use (TOU) energy charges. This cost 
+is determined by calculating the sum of products of net demand at each time step and TOU 
+energy prices at each time step.
 """
 function define_tou_energy_charge_objective!(m::JuMP.Model, obj::JuMP.AffExpr, sets::Sets)
     # Add time-of-use (TOU) energy charges to the objective function
@@ -11,7 +13,9 @@ end
 """
     define_demand_charge_objective!(m::JuMP.Model, obj::Jump.AffExpr, sets::Sets)
 
-TBW
+Adds a cost to the objective function for the different demand charges. This cost is 
+determined by calculating the sum of products of maximum demand for each demand-charge 
+period and the maximum demand price for that demand-charge period.
 """
 function define_demand_charge_objective!(m::JuMP.Model, obj::JuMP.AffExpr, sets::Sets)
     # Add demand charges to the objective function
@@ -25,7 +29,10 @@ end
         sets::Sets,
     )
 
-TBW
+Adds a revenue (i.e., subtracts a cost) to the objective function for the net energy 
+metering (NEM) program. This revenue is determined by calculating the sum of products of 
+net demand at each time step and NEM prices at eahc time step. To make it a revenue, this 
+sum is multiplied by -1.
 """
 function define_net_energy_metering_revenue_objective!(
     m::JuMP.Model,
